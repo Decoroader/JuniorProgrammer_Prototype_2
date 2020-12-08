23d225595722bb5bcb0e627c4e1b9cbaafa7c894
Decoroader <secbittree@gmail.com>
2020-12-08 18:45:41 +0200

    Lesson 2.1 - Player Positioning, 6, 7
    another edition

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
@@ -7,15 +8,22 @@ public class PlayerContoller : MonoBehaviour
     private float range = 10.0f;
     void Update()
     {
-        if(transform.position.x < -range)
-		{
-            transform.position = new Vector3(-range, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x > range)
-        {
-            transform.position = new Vector3(range, transform.position.y, transform.position.z);
-        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Math.Abs(transform.position.x) > range)
+            transform.position = new Vector3(Math.Sign(transform.position.x)*range, 
+                transform.position.y, transform.position.z);
+
+        //      if (transform.position.x < -range)
+        //      {
+        //          transform.position = new Vector3(-range, transform.position.y, transform.position.z);
+        //      }
+        //      if (transform.position.x > range)
+        //      {
+        //          transform.position = new Vector3(range, transform.position.y, transform.position.z);
+        //      }
+        //        horizontalInput = Input.GetAxis("Horizontal");
+        //        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
