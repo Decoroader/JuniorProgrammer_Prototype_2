0e624d4693b18b7c84666a6b7c812bd24ce47cd0
Decoroader <secbittree@gmail.com>
2020-12-08 16:47:32 +0200

    Lesson 2.1 - Player Positioning, 4, 5
    4.Get the user’s horizontal input, 5.Move the player left-to-right

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float horizontalInput;
+    public float speed = 10.0f;
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
