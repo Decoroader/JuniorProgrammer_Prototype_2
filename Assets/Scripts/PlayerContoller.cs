fde97f9f70038e6cdcdb3131666440b24cdf8e52
Decoroader <secbittree@gmail.com>
2020-12-08 17:00:23 +0200

    Lesson 2.1 - Player Positioning, 6, 7
    6.Keep the player inbounds, 7.Clean up your code and variables
    like in the lessons

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -4,8 +4,17 @@ public class PlayerContoller : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    private float range = 10.0f;
     void Update()
     {
+        if(transform.position.x < -range)
+		{
+            transform.position = new Vector3(-range, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > range)
+        {
+            transform.position = new Vector3(range, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
