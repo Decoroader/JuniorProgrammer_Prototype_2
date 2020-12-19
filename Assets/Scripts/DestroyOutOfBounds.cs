8ad393b1f32977e50a2499344743b9ca189718b4
Decoroader <secbittree@gmail.com>
2020-12-19 16:59:43 +0200

    Lesson 2.2 - Food Flight, 6, 8
    6.Make animals into prefabs, 8.Destroy animals offscreen

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,10 +3,13 @@
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30.0f;
+    private float lowBound = -10.0f;
 
     void Update()
     {
         if (transform.position.z > topBound)
+            Destroy(gameObject);
+        else if(transform.position.z < lowBound)
             Destroy(gameObject);
     }
 }
--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -2,21 +2,10 @@
 
 public class MoveForward : MonoBehaviour
 {
-    public float cookiSpeed = 13.0f;
-    private float topBound = 30.0f;
+    public float speed = 13.0f;
 
-    //void FixedUpdate()
-    //{
-    //    transform.Translate(Vector3.forward * Time.deltaTime * cookiSpeed);
-    //}
-    /*
-    why not for separately projectiles and animals; 
-    rename the script to Projectile Control and enter the following:
-    */void FixedUpdate()
-    {
-        transform.Translate(Vector3.forward* Time.deltaTime* cookiSpeed);
-        if (transform.position.z > topBound)
-            Destroy(gameObject);
-    }
-    
+	void FixedUpdate()
+	{
+		transform.Translate(Vector3.forward * Time.deltaTime * speed);
+	}
 }
