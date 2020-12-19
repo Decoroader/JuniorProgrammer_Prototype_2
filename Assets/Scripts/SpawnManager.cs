89c0bc74d6f53fdc8b170115bda660db2d7002f2
Decoroader <secbittree@gmail.com>
2020-12-19 17:32:28 +0200

    Lesson 2.3 - Random Animal Stampede, 2, 3
    2.Create a spawn manager, 3.Spawn an animal if S is pressed

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] AnimalPrefabs;
+    public int animalIndex;
+
+    private float maxZ = 20.0f;
+    private Vector3 newAnimalPosition;
+	private void Start()
+	{
+        newAnimalPosition = new Vector3(0, 0, maxZ);
+    }
+
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+            Instantiate(AnimalPrefabs[animalIndex], newAnimalPosition, AnimalPrefabs[animalIndex].transform.rotation);
+    }
+}
