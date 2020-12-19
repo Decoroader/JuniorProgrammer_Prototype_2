af7070381c67d092d0c84c8a3ca20daeda405a47
Decoroader <secbittree@gmail.com>
2020-12-19 16:30:28 +0200

    Lesson 2.2 - Food Flight, 4, 5, 7 another variant

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -3,9 +3,20 @@
 public class MoveForward : MonoBehaviour
 {
     public float cookiSpeed = 13.0f;
+    private float topBound = 30.0f;
 
-    void FixedUpdate()
+    //void FixedUpdate()
+    //{
+    //    transform.Translate(Vector3.forward * Time.deltaTime * cookiSpeed);
+    //}
+    /*
+    why not for separately projectiles and animals; 
+    rename the script to Projectile Control and enter the following:
+    */void FixedUpdate()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * cookiSpeed);
+        transform.Translate(Vector3.forward* Time.deltaTime* cookiSpeed);
+        if (transform.position.z > topBound)
+            Destroy(gameObject);
     }
+    
 }
