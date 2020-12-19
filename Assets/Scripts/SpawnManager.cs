bd27e566241d025d2c660a9aaef5c4cbb6b1faa1
Decoroader <secbittree@gmail.com>
2020-12-19 17:40:43 +0200

    Lesson 2.3 - Random Animal Stampede, 4, 5
    4.Spawn random animals from array, 5.Randomize the spawn location

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,18 +3,17 @@
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] AnimalPrefabs;
-    public int animalIndex;
-
+    private float maxAbsX = 20.0f;
     private float maxZ = 20.0f;
     private Vector3 newAnimalPosition;
-	private void Start()
-	{
-        newAnimalPosition = new Vector3(0, 0, maxZ);
-    }
-
+	
 	void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, AnimalPrefabs.Length);
+            newAnimalPosition = new Vector3(Random.Range(-maxAbsX, maxAbsX), 0, maxZ);
             Instantiate(AnimalPrefabs[animalIndex], newAnimalPosition, AnimalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
