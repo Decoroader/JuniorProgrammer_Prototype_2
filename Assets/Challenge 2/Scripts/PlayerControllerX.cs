6ab7859fdbcfb884808939ebb36b1b06366af78b
Decoroader <secbittree@gmail.com>
2020-12-20 16:36:19 +0200

    Challenge 2 7, 8
    bonuses w random time of spawn balls and lock time for button after keystroke

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,17 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
-    // Update is called once per frame
-    void Update()
+    private int lockButtonCounter = 0;
+    private readonly int lockButtonTime = 33;
+    void FixedUpdate()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(lockButtonCounter > 0)
+            lockButtonCounter--;
+        if (Input.GetKeyDown(KeyCode.Space) && (lockButtonCounter <= 0))
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lockButtonCounter = lockButtonTime;
         }
     }
 }
