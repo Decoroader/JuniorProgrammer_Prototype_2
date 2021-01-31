3c8e0b726f8b4a06eff0b33220abc870487f2ced
Decoroader <secbittree@gmail.com>
2021-01-31 08:06:26 +0200

    Lesson 6.1 - Project Optimization, 3
    3.Object Pooling

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -1,18 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30.0f;
-    private float lowBound = -10.0f;
+    private float topBound = 30;
+    private float lowerBound = -10;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
     void Update()
     {
         if (transform.position.z > topBound)
-            Destroy(gameObject);
-        else if (transform.position.z < lowBound)
+        {
+            // Instead of destroying the projectile when it leaves the screen
+            //Destroy(gameObject);
+
+            // Just deactivate it
+            gameObject.SetActive(false);
+
+        }
+        else if (transform.position.z < lowerBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
+
     }
 }
